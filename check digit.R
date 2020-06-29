00a6f456085611b1f66bebbec5b09b39b2508aeb
MinhTuan-Pham <66165364+MinhTuan-Pham@users.noreply.github.com>
2020-06-29 11:43:10 +0700

    Correct

--- check digit.R
+++ check digit.R
@@ -1,25 +1,19 @@
 #UPC check digit code
 upc_check_digit <- function(series)  {
   
-  odd <- numeric()
-  even <- numeric()
-  result <- numeric()
-  check_digit <- numeric()
-  reverse <- numeric()
-  yo <- numeric()
-  ye <- numeric()
-  
   reverse <- rev(series)[-1]
   
-  for (i in 1:length(reverse)) {
+  i <- 1:length(reverse)
      yo <- reverse[i%%2 != 0]
      ye <- reverse[i%%2 == 0]
-  }
+  
   odd <- sum(yo)
   even <- sum(ye)
-  result <- odd + even
+  result <- odd*3 + even
   
-  ifelse((result %% 10) == 0, check_digit <- 0, check_digit <- 10 - (result %% 10))
+  if (result %% 10 == 0) {
+    check_digit <- 0 
+  } else { check_digit <- 10 - (result %% 10)}
   
   check_digit
 }
